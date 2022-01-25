6b58e6fdc391364aaad86f22b3ac255e1dde5388
Thel Nu Aein <thelnuaein5450.com>
2022-01-25 13:39:01 +0630

    first

---
+++ database/migrations/2022_01_25_064759_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('slug');
+            $table->string('photo');
+            $table->string('price');
+            $table->longText('description');
+            $table->text('excerpt');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
