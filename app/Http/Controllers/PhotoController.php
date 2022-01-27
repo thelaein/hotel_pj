4b4f2512a322f73f774c65c05272d05047f31f4c
thinkabyaroo <thinkabyaroo2911@gmail.com>
2022-01-27 14:23:40 +0630

    edit photo

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -116,6 +116,10 @@ class PhotoController extends Controller
      */
     public function destroy(Photo $photo)
     {
-        //
+        Storage::delete('public/photo'.$photo->name);
+        Storage::delete('public/thumbnail'.$photo->name);
+        $photo->delete();
+
+        return redirect()->back();
     }
 }
